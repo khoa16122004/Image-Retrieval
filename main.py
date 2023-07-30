88c1dca6d5a0f056fa32e284c8fb0580d8d657d3
KhoaTN <110011154+khoa16122004@users.noreply.github.com>
2023-07-30 21:25:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ from preprocessing import load_data, image_db
 from add_search import faiss_add, search, visual_result
 import torch
 import clip
-
+import faiss
 
 
 def encode_text(input_text, model):
-  tokenized_text = torch.cat([clip.tokenize(input_text)]).to(device)
-  with torch.no_grad():
-    text_feature = model.encode_text(tokenized_text)
-  text_feature /= text_feature.norm(dim=-1, keepdim=True)
-  return text_feature
-
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    tokenized_text = torch.cat([clip.tokenize(input_text)]).to(device)
+    with torch.no_grad():
+        text_feature = model.encode_text(tokenized_text)
+    text_feature /= text_feature.norm(dim=-1, keepdim=True)
+    return text_feature
 
 def main():
-  image_dataset = load_data("/content/images")
-  image_features,  model = image_db(image_dataset)
-  index = faiss.IndexFlatL2(image_features.shape[1], image_features)
-  text = input()
-  text_encode = encode_text(text, model)
-  D, I = search(text_encode, image_features, 10)
-  visual_result(I, image_dataset)
+    image_dataset = load_data(r"D:\AIC\animals_dataset")
+    model, image_features = image_db(image_dataset)
+    index = faiss_add(image_features[0].shape[1], image_features)
+    print(type(index))
+    text = input("Your querry: ")
+    text_encode = encode_text(text, model)
+    D, I = search(text_encode, index, 10)
+    visual_result(I, image_dataset)
 
-  
+main()
